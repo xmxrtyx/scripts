014141fe6e15dd7d2241a14d3fda9374c7de2354
Amartya <56784444+amartyag2004@users.noreply.github.com>
2020-04-28 23:17:23 +0530

    Create angler.sh

---
+++ angler.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Script to clone angler device sources
+#
+#Stole from Rahul xd
+
+# Device tree
+echo -e "\n================== Cloning Device tree for angler ==================\n"
+git clone https://github.com/amartyag2004/android_device_huawei_angler-10 device/huawei/angler
+
+# Kernel tree
+echo -e "\n================== Cloning Kernel tree for angler ==================\n"
+git clone https://github.com/amartyag2004/android_kernel_huawei_angler-10 kernel/huawei/angler
+
+# Vendor tree
+echo -e "\n================== Cloning Vendor tree for angler ==================\n"
+git clone https://github.com/amartyag2004/android_vendor_huawei-10 vendor/huawei/angler
