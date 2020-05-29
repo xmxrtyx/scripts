698aa7d0a57a888c4f22ac0bd2cf36b387be629b
Amartya <garikapatiamartya@gmail.com>
2020-05-29 11:08:10 +0530

    Update gitsetup.sh

--- gitsetup.sh
+++ gitsetup.sh
@@ -5,10 +5,12 @@
 # Username
 echo -e "\n================== Adding Username ==================\n"
 git config --global user.name "xmxrtyx"
+echo -e "\n Added!\n"
 
 #Email
 echo -e "\n==================   Adding Email   ==================\n"
 git config --global user.email "garikapatiamartya@gmail.com"
+echo -e "\n Added!\n"
 
 #List Settings
 echo -e "\n==================   List Settings   ==================\n"
