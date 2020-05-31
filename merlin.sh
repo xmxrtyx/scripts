82437830556029007f8c7e875fa89463b281c550
Amartya <garikapatiamartya@gmail.com>
2020-05-31 09:34:53 +0530

    Create merlin.sh

---
+++ merlin.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Script to clone angler device sources
+
+
+# Device tree
+echo -e "\n================== Cloning Device tree for merlin ==================\n"
+git clone https://github.com/xmxrtyx/device_motorola_merlin device/motorola/merlin
+
+# Common Tree
+echo -e "\n================== Cloning Common tree for merlin ==================\n"
+git clone https://github.com/xmxrtyx/android_device_motorola_msm8916-common device/motorola/msm8916-common
+
+# Kernel tree
+echo -e "\n================== Cloning Kernel tree for merlin ==================\n"
+git clone https://github.com/xmxrtyx/kernel_motorola_msm8916-1 kernel/motorola/msm8916
+
+# Vendor tree
+echo -e "\n================== Cloning Vendor tree for merlin ==================\n"
+git clone https://github.com/xmxrtyx/vendor_motorola vendor/motorola/
+
+#End
+echo -e "\n============== Thank you for using this, happy building ==============\n"
