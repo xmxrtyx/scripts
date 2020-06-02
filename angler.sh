412a3e283fb3742325c8a42c483178cc1fa92c85
Amartya <garikapatiamartya@gmail.com>
2020-06-02 13:49:59 +0530

    Update angler.sh

--- angler.sh
+++ angler.sh
@@ -6,7 +6,11 @@
 
 # Device tree
 echo -e "\n================== Cloning Device tree for angler ==================\n"
-git clone https://github.com/xmxrtyx/android_device_huawei_angler-10 device/huawei/angler
+git clone https://github.com/xmxrtyx/android_device_huawei_angler device/huawei/angler
+
+# Common tree
+echo -e "\n================== Cloning Common tree for angler ==================\n"
+git clone https://github.com/xmxrtyx/android_device_huawei_angler-common device/huawei/angler-common
 
 # Kernel tree
 echo -e "\n================== Cloning Kernel tree for angler ==================\n"
