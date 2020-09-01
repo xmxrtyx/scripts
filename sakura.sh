14bdaf3782f97af2be206c203732120627502d90
Amartya <garikapatiamartya@gmail.com>
2020-09-02 00:04:20 +0530

    Update sakura.sh

--- sakura.sh
+++ sakura.sh
@@ -12,7 +12,8 @@ git clone https://github.com/Sakura-Devices/vendor_xiaomi_ginkgo vendor/xiaomi/g
 rm -rf hardware/qcom-caf/sm8150/display && rm -rf hardware/qcom-caf/sm8150/media && rm -rf hardware/qcom-caf/sm8150/audio
 git clone https://github.com/trinket-devs/hardware_qcom-caf_sm8150_display.git -b ten hardware/qcom-caf/sm8150/display && git clone https://github.com/trinket-devs/hardware_qcom-caf_sm8150_audio.git -b ten hardware/qcom-caf/sm8150/audio && git clone https://github.com/trinket-devs/hardware_qcom-caf_sm8150_media.git -b ten hardware/qcom-caf/sm8150/media
 git clone https://github.com/trinket-devs/vendor_miuicamera.git -b 10.0 vendor/miuicamera && git clone https://github.com/trinket-devs/proton-clang.git prebuilts/clang/host/linux-x86/clang-12.0
-export CCACHE_EXEC=$(which ccache) && export USE_CCACHE=1 && export CCACHE_DIR=/home/wtfyuki/ccache && ccache -M 50G
+export LC_ALL=C
+export USE_CCACHE=true
 . b*/e*
 lunch lineage_ginkgo-userdebug
 mka bacon -j8 | tee
