4a9459c8a7760af53c4282a99dc94dc2e7dbeec8
Amartya <garikapatiamartya@gmail.com>
2020-09-16 00:36:15 +0530

    Update sakura.sh

--- sakura.sh
+++ sakura.sh
@@ -2,6 +2,10 @@ wget https://raw.githubusercontent.com/xmxrtyx/scripts/master/gitsetup.sh
 bash gitsetup.sh
 wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh
 bash android_build_env.sh
+wget https://www.dropbox.com/s/w65lffvkkqvvj93/gdrive?dl=1
+mv gdrive?dl=1 gdrive
+chmod +x gdrive
+./gdrive list
 mkdir sakura
 cd sakura
 repo init --depth=1 -u git://github.com/ProjectSakura/android.git -b 10
